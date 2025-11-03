50b8836226d9191897e0e183509a8048f67673e5
Juanez <ezjuanify@gmail.com>
2025-11-04 02:26:09 +0800

    JAR-#2: Clean up excess themes

--- wp-content/themes/rozer/inc/admin/customizer/blog/blog.php
+++
@@ -1,7 +0,0 @@
-<?php
-Kirki::add_panel( 'blog', array(
-    'priority'    => 55,
-    'title'       => esc_html__( 'Blog', 'rozer' ),
-) );
-require_once dirname( __FILE__ ).'/blog-archive.php';
-require_once dirname( __FILE__ ).'/blog-single.php';
--- wp-content/themes/rozer/inc/admin/customizer/styles/styles.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-Kirki::add_panel( 'styles', array(
-    'priority'    => 49,
-    'title'       => esc_html__( 'Styles', 'rozer' ),
-) );
-require_once dirname( __FILE__ ).'/styles-color.php';
-require_once dirname( __FILE__ ).'/styles-typography.php';
